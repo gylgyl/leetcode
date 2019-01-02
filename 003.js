6eb597c9cf5aeadf4d1856670c1da3d9f416a6b7
GreatGhost <1609401009@qq.com>
2019-01-02 18:59:12 +0800

    3

---
+++ 003.js
@@ -0,0 +1,47 @@
+/*
+给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+
+示例 1:
+
+输入: “abcabcbb” 输出: 3 解释: 因为无重复字符的最长子串是 “abc”，所以其长度为 3。
+
+示例 2:
+
+输入: “bbbbb” 输出: 1 解释: 因为无重复字符的最长子串是 “b”，所以其长度为 1。
+
+示例 3:
+
+输入: “pwwkew” 输出: 3 解释: 因为无重复字符的最长子串是 “wke”，所以其长度为 3。
+
+ 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+*/
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+  if(!s){
+    return 0
+  }
+  var tempResult = '';
+  var result = '';
+
+  for(var i=0; i<s.length; i++ ){
+    if(tempResult.indexOf(s[i]) === -1){
+      tempResult += s[i];
+      if(tempResult.length > result.length) result = tempResult
+    }else{
+      if(tempResult.length > result.length){
+        result = tempResult
+      }
+      let index = tempResult.indexOf(s[i])
+      tempResult = tempResult.slice(index+1) + s[i] 
+    }
+    console.log('temp:',tempResult);
+    console.log('result:',result);
+  }
+  return result.length
+};
+
+
+console.log('lengthOfLongestSubstring()',lengthOfLongestSubstring('asd459055werqg'));
